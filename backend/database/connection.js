056e185ae00b6022f61751fbe6cb38659e677d66
Kanishkagarg16 <kanishkagarg16dec2005@gmail.com>
2026-06-23 02:25:30 +0530

    Fix MongoDB dbName env variable in connection.js

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connection = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI, {
-            dbName: "employease"
+            dbName: process.env.recruitment || "employease"
         });
         console.log("[DB] Connected to database successfully");
         return true;
